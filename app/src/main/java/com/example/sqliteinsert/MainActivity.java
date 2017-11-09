1e6e718ba98b43ca2004005ac5a18dfa8a33db59
Christian Richters <c.v.kietan@outlook.com>
2017-11-09 11:18:05 +0100

    Final Commit

--- app/src/main/java/com/example/sqliteinsert/MainActivity.java
+++ app/src/main/java/com/example/sqliteinsert/MainActivity.java
@@ -13,14 +13,27 @@ public class MainActivity extends AppCompatActivity {
     EditText Name, Pass;
     myDbAdapter helper;
     Context context;
-    Button ShowData;
+    Button ShowData, addUser, btnUpdate, btnDelete;
+    EditText editOldField, editUpdateField, editDel;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context= getApplicationContext();
         Name= (EditText) findViewById(R.id.editName);
         Pass= (EditText) findViewById(R.id.editPass);
+        editOldField = (EditText) findViewById(R.id.editText3);
+        editUpdateField = (EditText) findViewById(R.id.editText5);
+        editDel = (EditText) findViewById(R.id.editText6);
         ShowData= (Button) findViewById(R.id.button2);
+        addUser= (Button) findViewById(R.id.button);
+        btnUpdate = (Button) findViewById(R.id.button3);
+        btnDelete = (Button) findViewById(R.id.button4);
+        
+        
+        
+
         helper = new myDbAdapter(this);
 
         ShowData.setOnClickListener(new View.OnClickListener() {
@@ -31,16 +44,48 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button addUser = (Button) findViewById(R.id.button);
+        //Button addUser = (Button) findViewById(R.id.button);
         addUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addUser(view);
+                addUser(view, context);
             }
         });
+        
+        //ButtonUpdateUser
+        btnUpdate.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                try {
+                    String oldField = editOldField.getText().toString();
+                    String Update = editUpdateField.getText().toString();
+                    helper.UpdateData(oldField, Update);
+                    Toast.makeText(MainActivity.this, "Update Successful", Toast.LENGTH_SHORT).show();
+                }catch (Exception e){
+                    Toast.makeText(MainActivity.this, "Update Unsuccessful", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    String name = editDel.getText().toString();
+                    helper.deleteData(name);
+                    Toast.makeText(MainActivity.this, "Delete Successful", Toast.LENGTH_SHORT).show();
+
+                }catch(Exception e){
+                    Toast.makeText(MainActivity.this, "Delete Unsuccessful", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+       
+
     }
 
-    public void addUser(View view)
+    public void addUser(View view, Context context)
     {
         Toast.makeText(this,"Running", Toast.LENGTH_LONG).show();
         String t1 = Name.getText().toString();
--- app/src/main/java/com/example/sqliteinsert/myDbAdapter.java
+++ app/src/main/java/com/example/sqliteinsert/myDbAdapter.java
@@ -5,8 +5,11 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import com.example.sqliteinsert.UserContract;
+import com.example.sqliteinsert.UserContract.Contract.UserEntity;
 
 import static com.example.sqliteinsert.UserContract.Contract.UserEntity.TABLE_NAME;
+import static com.example.sqliteinsert.UserContract.Contract.UserEntity.USER_NAME;
 
 public class myDbAdapter  {
 
@@ -22,7 +25,7 @@ public class myDbAdapter  {
         ContentValues contentValues = new ContentValues();
         contentValues.put(UserContract.Contract.UserEntity.USER_NAME, name);
         contentValues.put(UserContract.Contract.UserEntity.USER_PWD, password);
-        long id = db.insert(UserContract.Contract.DATABASE_NAME, null, contentValues);
+        long id = db.insert(UserContract.Contract.UserEntity.TABLE_NAME, null, contentValues);
         return id;
     }
 
@@ -33,10 +36,24 @@ public class myDbAdapter  {
         return data;
     }
 
+    public void UpdateData(String oldField, String newField){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        String Query = "Update "+ TABLE_NAME + " set " + USER_NAME + " = '" + newField + "' where "+
+                USER_NAME + " = " + "'" + oldField + "'";
+        db.execSQL(Query);
+    }
+
+    public void deleteData(String Data){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        String Query = "DELETE FROM " + TABLE_NAME + " WHERE " + USER_NAME +
+                " = " + "'" + Data + "'";
+        db.execSQL(Query);
+    }
+
     static class myDbHelper extends SQLiteOpenHelper {
         private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
-                + UserContract.Contract.UserEntity.UID + " INTEGER PRIMARY KEY AUTOINCREMENT ,"
-                + UserContract.Contract.UserEntity.USER_NAME + " VARCHAR(225), "
+                + UserContract.Contract.UserEntity.UID + " INTEGER PRIMARY KEY AUTOINCREMENT , "
+                + UserContract.Contract.UserEntity.USER_NAME + " VARCHAR(225) , "
                 + UserContract.Contract.UserEntity.USER_PWD + " VARCHAR(225));";
         private static final String DROP_TABLE = "DROP TABLE IF EXISTS "
                 + TABLE_NAME;
